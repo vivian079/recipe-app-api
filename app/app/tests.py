04e88039b8f9afe662474c39fe0ebe304e576010
yujie79 <yujiezhang079@gmail.com>
2023-10-14 14:58:39 -0500

    commit

---
+++ app/app/tests.py
@@ -0,0 +1,14 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+         res = calc.subtract(10, 15)
+
+         self.assertEqual(res, 5)
